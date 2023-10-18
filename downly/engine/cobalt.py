243283a0759c12c98692c617128d220c73b91ac9
Anil Chauhan <anilchauhan@mogiio.com>
2023-10-18 05:32:40 +0530

    handling failed api response cobalt api were kept failing in random time, `CobaltEngine.MAX_RETRIES = 5` for better user response, it will keep try for MAX_RERIES times until it dont get success!

--- downly/engine/cobalt.py
+++ downly/engine/cobalt.py
@@ -20,10 +20,35 @@ def get_config(config: dict):
 
 
 class CobaltEngine:
+
+
+    MAX_RETRIES = 5
+
     def __init__(self, cobalt_base_url: str = configs.get('cobalt_base_url')):
         self.cobalt_base_url = cobalt_base_url
 
     def download(self, config: dict):
+        """
+        Download video from cobalt
+        that's keep retrying if failed
+        for MAX_RETRIES times
+        :param config:
+        :return: dict
+        """
+        try:
+            r = self.download_(config)
+            CobaltEngine.MAX_RETRIES = 5  # reset
+            return r
+        except Exception as e:
+            print(f'Error while downloading {config.get("url")}\n')
+            CobaltEngine.MAX_RETRIES -= 1
+            print(f'number of retries left: {CobaltEngine.MAX_RETRIES}\n')
+            if CobaltEngine.MAX_RETRIES == 0: # if no retries left
+                CobaltEngine.MAX_RETRIES = 5 # reset
+                raise Exception(e)
+            return self.download(config)
+
+    def download_(self, config: dict):
         r = httpx.post(
             f"{self.cobalt_base_url}/api/json",
             json=get_config(config),
