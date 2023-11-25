0a261bce38e331da145151c46b3c66975a17d268
anil <anilchauhanxda@gmail.com>
2023-11-25 12:42:04 +0530

    using uvloop drop-in replacement of the built-in asyncio event loop

--- downly/__main__.py
+++ downly/__main__.py
@@ -1,4 +1,6 @@
 import importlib
+import uvloop
+
 from .downly import Downly
 from downly.database import ALL_DATABASES_MODULES
 
@@ -7,4 +9,5 @@ for module in ALL_DATABASES_MODULES:
     imported_module = importlib.import_module("downly.database." + module)
 
 if __name__ == "__main__":
+    uvloop.install()
     Downly().run()  # running bot
