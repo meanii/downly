5c4ef820b66409a329d4234c3ac1e10f461ebf84
anil <anilchauhanxda@gmail.com>
2024-03-19 17:55:46 +0530

    adding last 24 hours active users and chat

--- downly/plugins/stats.py
+++ downly/plugins/stats.py
@@ -8,6 +8,8 @@ from downly.database.users_sql import (
     count_chats,
     count_last_24_hours_users,
     count_last_24_hours_chats,
+    count_last_24_hours_active_users,
+    count_last_24_hours_active_chats,
 )
 from downly.database.downloads_sql import count_downloads, count_last_24_hours_downloads
 
@@ -26,8 +28,8 @@ async def stats(_, message):
         f"We currently have `{count_users()}` users spread across `{count_chats()}` chats. "
         f"The total download count stands at `{count_downloads()}`.\n\n"
         f"In the last 24 hours alone, there were `{count_last_24_hours_users()}` new users joining, "
-        f"engaging in `{count_last_24_hours_chats()}` chats, and `{count_last_24_hours_downloads()}` downloads."
+        f"engaging in `{count_last_24_hours_chats()}` chats, and `{count_last_24_hours_downloads()}` downloads. "
+        f"Additionally, there were `{count_last_24_hours_active_users()}` active users and `{count_last_24_hours_active_chats()}` active chats in the last 24 hours."
     )
 
     await message.reply_text(stats_message, disable_web_page_preview=True)
-
